442efc148ca3bcffd82db29508c2ba47919b2deb
Pruthvi P <pruthvi@ixigo.com>
2020-06-11 11:27:03 +0530

    Add build files for client

---
+++ build/precache-manifest.39174410c3a2bde406bf16ec96207ca0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1851249ae1aa45c00fdc8083148632",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c54824b9095ea4d31940",
+    "url": "/static/css/main.1d285a34.chunk.css"
+  },
+  {
+    "revision": "9daf1f45d489db7a4611",
+    "url": "/static/js/2.234805bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.234805bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54824b9095ea4d31940",
+    "url": "/static/js/main.b879f554.chunk.js"
+  },
+  {
+    "revision": "6a06dd8c58e6064993b5",
+    "url": "/static/js/runtime-main.83421ab0.js"
+  }
+]);
